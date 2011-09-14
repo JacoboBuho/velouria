ba46aa13a89c1e7b39e4923b89e03fd0b60fc79c
JacoboBuho <a.g.jacobo@gmail.com>
2011-09-14 11:28:39 -0500

    my velouria

---
+++ app/controllers/hola_controller.rb
@@ -0,0 +1,5 @@
+class HolaController < ApplicationController
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Velouria::Application.config.secret_token = 'dbfcc90f7bd62a8db048e6cfa3c31dd37f67fc5af24e18003dbaca2a7d35b2d6d137fbb09bfd73c9474475f5343f1ab5deb7e648c5c171ed35fe70ba09ae1bb7'
